1c87780eb0e058e7bf0a21714fe2ea6c2433ffbd
Samiaerum <121792974+Samiaerum@users.noreply.github.com>
2023-03-12 09:59:11 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+ var firstname="samia";
+ var secondname="samia erum"
+
+/////////////////////////////////////////////////////////////////////////////
+var message="hello world"
+alert(message);
+
+var name="samia"
+var age="18 year old"
+var admited="enroll in web development and mobile applicaton course"
+alert(name);
+alert( age);
+alert(admited);
+
+
+
+/////////////////////////////////////////////////////////////////////////
+var abc="pizza \n Pizz \n Piz \n Pi \n P"
+alert(abc);
+
+
+
+////////////////////////////////////////////////////////////////////////////
+var x =" my email address is "
+ var y ="example@example.com"
+ var z =x+y;
+alert(z);
+
+
+/////////////////////////////////////////////////////////////////////////////
+var xyz =" i m trying to learn from the book A smarter way to learn JavaScript"
+alert(xyz);
+
+
+
+////////////////////////////////////////////////////////////////////////////
+
+//  var myname ="yah! i can write html content through js"
+ 
+//  document.write="myname"
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+var efg ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(efg);
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+var age1 ="                                             i m 19 years old                                         "
+alert(age1);
+
+
+
+/////////////////////////////////////////////////////////////////
+
+
+
+var visitor=14;
+alert("you have visited this site "+14+"times")
+
+
+///////////////////////////////////////////////////////////////
+
+// var birthyear=2003
+// document.write("you have" "+birthyear+"" years old");
+
+
+
+//////////////////////////////////////////////////////////////////////
+
+
+var name="samia";
+var quantity=6;
+var product="t-shirt";
+document.write(name+" ordered "+quantity+" " +product+ "  from xyz clothing store");
